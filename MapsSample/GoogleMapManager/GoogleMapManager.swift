2825dbfdc6d68f91f441de6cf604cd480c71cb16
Rajesh_485 <rajapple1993@gmail.com>
2017-08-19 10:38:58 +0530

    Goolemap manager has been added

---
+++ MapsSample/GoogleMapManager/GoogleMapManager.swift
@@ -0,0 +1,13 @@
+//
+//  GoogleMapManager.swift
+//  MapsSample
+//
+//  Created by kasula's on 19/08/17.
+//  Copyright © 2017 kasula's. All rights reserved.
+//
+
+import UIKit
+
+class GoogleMapManager: NSObject {
+
+}
